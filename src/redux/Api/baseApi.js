2efc1127e78debdf7bd8e76c5f27b3329057b350
Ishaq ahammad <ishaqahammad60@gmail.com>
2025-10-29 18:26:22 +0600

    commit change

--- src/redux/Api/baseApi.js
+++ src/redux/Api/baseApi.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const baseApi = createApi({
     reducerPath: 'baseApi',
-    baseQuery: fetchBaseQuery({baseUrl:import.meta.env.VITE_API_BASE_URL, }),
+    baseQuery: fetchBaseQuery({baseUrl:import.meta.env.VITE_API_BASE_URL}),
     tagTypes: ['Books'],
     endpoints: (builder) => ({
         getBooks: builder.query({
--- src/redux/Api/baseApi.ts
+++ src/redux/Api/baseApi.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const baseApi = createApi({
   reducerPath: 'baseApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000/api' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'baseUrl:import.meta.env.VITE_API_BASE_URL'}),
   tagTypes: ['Books'],
   endpoints: (builder) => ({
     getBooks: builder.query({
